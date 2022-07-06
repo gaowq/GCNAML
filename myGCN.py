d92bf4095979677c601aa9119ba953ac4234a0ee
gaowq <1138946467@qq.com>
2022-07-06 21:05:33 +0800

    删减

---
+++ myGCN.py
@@ -0,0 +1,188 @@
+import numpy
+import os
+import pandas
+from sklearn.metrics import f1_score, precision_score, recall_score
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import Parameter
+from torch_geometric.data import Data,DataLoader
+from torch_geometric.nn import GCNConv
+from torch_geometric.utils import to_undirected
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+#1.获取数据源
+df_edge=pandas.read_csv('D:\code\d4\elliptic_txs_edgelist.csv')
+df_class=pandas.read_csv('D:\code\d4\elliptic_txs_classes.csv')
+df_features=pandas.read_csv('D:\code\d4\elliptic_txs_features.csv',header=None)
+
+#2.数据处理、关联
+df_features.columns = ['id', 'time step'] + [f'trans_feat_{i}' for i in range(93)] + [f'agg_feat_{i}' for i in range(72)]
+
+all_nodes = list(set(df_edge['txId1']).union(set(df_edge['txId2'])).union(set(df_class['txId'])).union(set(df_features['id'])))
+nodes_df = pandas.DataFrame(all_nodes,columns=['id']).reset_index()
+
+df_edge = df_edge.join(nodes_df.rename(columns={'id':'txId1'}).set_index('txId1'),on='txId1',how='inner') \
+       .join(nodes_df.rename(columns={'id':'txId2'}).set_index('txId2'),on='txId2',how='inner',rsuffix='2') \
+       .drop(columns=['txId1','txId2']) \
+       .rename(columns={'index':'txId1','index2':'txId2'})
+df_edge.head()
+
+df_class = df_class.join(nodes_df.rename(columns={'id':'txId'}).set_index('txId'),on='txId',how='inner') \
+        .drop(columns=['txId']).rename(columns={'index':'txId'})[['txId','class']]
+df_class.head()
+
+
+df_features = df_features.join(nodes_df.set_index('id'),on='id',how='inner') \
+        .drop(columns=['id']).rename(columns={'index':'id'})
+df_features = df_features [ ['id']+list(df_features.drop(columns=['id']).columns) ]
+df_features.head()
+
+df_edge_time = df_edge.join(df_features[['id','time step']].rename(columns={'id':'txId1'}).set_index('txId1'),on='txId1',how='left',rsuffix='1') \
+.join(df_features[['id','time step']].rename(columns={'id':'txId2'}).set_index('txId2'),on='txId2',how='left',rsuffix='2')
+df_edge_time['is_time_same'] = df_edge_time['time step'] == df_edge_time['time step2']
+df_edge_time_fin = df_edge_time[['txId1','txId2','time step']].rename(columns={'txId1':'source','txId2':'target','time step':'time'})
+
+
+node_label = df_class.rename(columns={'txId':'nid','class':'label'})[['nid','label']].sort_values(by='nid').merge(df_features[['id','time step']].rename(columns={'id':'nid','time step':'time'}),on='nid',how='left')
+node_label['label'] =  node_label['label'].apply(lambda x: '3'  if x =='unknown' else x).astype(int)-1
+node_label.head()
+
+
+merged_nodes_df = node_label.merge(df_features.rename(columns={'id':'nid','time step':'time'}).drop(columns=['time']),on='nid',how='left')
+merged_nodes_df.head()
+
+#3.训练、测试数据加载
+train_dataset=[]
+test_dataset=[]
+for i in range(49):
+    nodes_df_tmp=merged_nodes_df[merged_nodes_df['time']==i+1].reset_index()
+    nodes_df_tmp['index']=nodes_df_tmp.index
+    df_edge_tmp = df_edge_time_fin.join(nodes_df_tmp.rename(columns={'nid':'source'})[['source','index']].set_index('source'),on='source',how='inner')\
+        .join(nodes_df_tmp.rename(columns={'nid':'target'})[['target','index']].set_index('target'),on='target',how='inner',rsuffix='2') \
+        .drop(columns=['source','target']) \
+        .rename(columns={'index':'source','index2':'target'})
+    x = torch.tensor(numpy.array(nodes_df_tmp.sort_values(by='index').drop(columns=['index','nid','label'])), dtype=torch.float)
+    edge_index = torch.tensor(numpy.array(df_edge_tmp[['source','target']]).T, dtype=torch.long)
+    edge_index = to_undirected(edge_index)
+    mask = nodes_df_tmp['label']!=2
+    y=torch.tensor(numpy.array(nodes_df_tmp['label']))
+
+    #1-34训练，之后测试
+    if i+1<35:
+        data = Data(x=x,edge_index=edge_index, train_mask=mask, y=y)
+        train_dataset.append(data)
+    else:
+        data = Data(x=x,edge_index=edge_index, test_mask=mask, y=y)
+        test_dataset.append(data)
+
+train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+
+
+#4.图卷积神经网络构建
+class GCN(torch.nn.Module):
+    def __init__(self, num_node_features, hidden_channels, use_skip=False):
+        super(GCN, self).__init__()
+        self.conv1 = GCNConv(num_node_features, hidden_channels[0])
+        self.conv2 = GCNConv(hidden_channels[0], 2)
+        self.use_skip = use_skip
+        if self.use_skip:
+            self.weight = nn.init.xavier_normal_(Parameter(torch.Tensor(num_node_features, 2)))
+
+
+    def forward(self, data):
+        x = self.conv1(data.x, data.edge_index)
+        x = x.relu()
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.conv2(x, data.edge_index)
+        if self.use_skip:
+            x = F.softmax(x+torch.matmul(data.x, self.weight), dim=-1)
+        else:
+            x = F.softmax(x, dim=-1)
+        return x
+
+    def embed(self, data):
+        x = self.conv1(data.x, data.edge_index)
+        return x
+
+model = GCN(num_node_features=data.num_node_features ,hidden_channels=[100])
+model.to(device)
+
+patience = 50
+lr = 0.001
+epoches = 1000
+
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+criterion = torch.nn.CrossEntropyLoss(weight=torch.tensor([0.7,0.3]).to(device))
+
+
+#5.训练和测试
+train_losses=[]
+val_losses =[]
+accuracies=[]
+if1=[]
+precisions=[]
+recalls=[]
+iterations=[]
+
+for epoch in range(epoches):
+    
+    model.train()
+    train_loss = 0
+    for data in train_loader:
+        data = data.to(device)
+        optimizer.zero_grad()
+        out = model(data)
+        loss = criterion(out[data.train_mask], data.y[data.train_mask].to(torch.int64))
+        _, pred = out[data.train_mask].max(dim=1)
+        loss.backward()
+        train_loss += loss.item() * data.num_graphs
+        optimizer.step()
+    train_loss /= len(train_loader.dataset)
+    
+
+    if (epoch+1)%50==0:
+        model.eval()
+        ys, preds = [], []
+        val_loss = 0
+        for data in test_loader:
+            data = data.to(device)
+            out = model(data)
+            loss = criterion(out[data.test_mask], data.y[data.test_mask].to(torch.int64))
+            val_loss += loss.item() * data.num_graphs
+            _, pred = out[data.test_mask].max(dim=1)
+            ys.append(data.y[data.test_mask].cpu())
+            preds.append(pred.cpu())
+
+        y, pred = torch.cat(ys, dim=0).numpy(), torch.cat(preds, dim=0).numpy()
+        val_loss /= len(test_loader.dataset)
+        f1 = f1_score(y, pred, average=None)
+        mf1 = f1_score(y, pred, average='micro')
+        #如果为None，则返回每个类的分数。否则，这决定了对数据进行平均的类型:
+        #“micro”: 通过计算总真阳性、假阴性和假阳性来全局计算指标。
+
+        precision = precision_score(y, pred, average=None)
+        recall = recall_score(y, pred, average=None)
+
+        iterations.append(epoch+1)
+        train_losses.append(train_loss)
+        val_losses.append(val_loss)
+        if1.append(f1[0])
+        accuracies.append(mf1)
+        precisions.append(precision[0])
+        recalls.append(recall[0])
+
+        print('Epoch: {:02d}, Train_Loss: {:.4f}, Val_Loss: {:.4f}, Precision: {:.4f}, Recall: {:.4f}, Illicit f1: {:.4f}, F1: {:.4f}'.format(epoch+1, train_loss, val_loss, precision[0], recall[0], f1[0], mf1))
+
+a,b,c,d = train_losses, val_losses, if1, accuracies
+plt.plot(numpy.array(a), 'r', label='Train loss')
+plt.plot(numpy.array(b), 'g', label='Valid loss')
+plt.plot(numpy.array(c), 'black', label='Illicit F1')
+plt.plot(numpy.array(d), 'orange', label='F1')
+plt.ylim([0,1.0])
+plt.show()
